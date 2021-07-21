10cd8f09968eae9c04298439f65a0a7eb6c8cd82
Sachin Kumar <83978025+sachinkp92@users.noreply.github.com>
2021-07-21 15:55:37 +0530

    Add files via upload

---
+++ Project -Matlib Game.py
@@ -0,0 +1,8 @@
+color = input("Enter a color :")
+plural_noun = input("Enter a plural noun :")
+sport_name = input("Enter a sport name :")
+
+print("Roses are " + color)
+print(plural_noun + " is blue")
+print("Best sport is " + sport_name)
+
